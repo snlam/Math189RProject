181af5f20494e181a14b6f8dbdba514c3a83c05a
malmorarios <43325603+malmorarios@users.noreply.github.com>
2020-07-04 21:00:38 -0700

    Update finalProject.py

--- finalProject.py
+++ finalProject.py
@@ -1,2 +1,5 @@
 import gensim, numpy, matplotlib, pandas
-
+import gzip, json
+all_lines = []
+for line in gzip.open("gutenberg-poetry-v001.ndjson.gz"):
+     all_lines.append(json.loads(line.strip()))
