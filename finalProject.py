fe31e585de2822fea605fffbfa02f8aea0cce2f7
Shanni Lam <snlam@hmc.edu>
2020-07-04 22:25:36 -0700

    Delete finalProject.py

--- finalProject.py
+++
@@ -1,5 +0,0 @@
-import gensim, numpy, matplotlib, pandas
-import gzip, json
-all_lines = []
-for line in gzip.open("gutenberg-poetry-v001.ndjson.gz"):
-     all_lines.append(json.loads(line.strip()))
