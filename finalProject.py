6acb49a2e225b1700ede7d1d15f60b3a2a8ae000
malmorarios <43325603+malmorarios@users.noreply.github.com>
2020-07-08 21:57:47 -0700

    Update finalProject.py

--- finalProject.py
+++ finalProject.py
@@ -1,169 +1,193 @@
-# import pandas, matplotlib
-
-import gensim, numpy
-import gzip, json
-import random
-import math
-
 # for timing our program
 import timeit
+start = timeit.default_timer()
 
-# who knows if we're using this; but goes with the list comprehension step
-import re
-
-# for natural language processing things
+# for natural language processing things (i.e. word2Vec)
 import spacy
-spacy.prefer_gpu()
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg") # sm / md / lg refers to size of the modules; lg [788 MB] vs sm [10 MB] will be accurate, but loads a lot slower
+wordDim = 300    # all vector representations of words in spacy vocabulary are of dimension 300
+
+# for mathy operations
+import math 
+import numpy as np
+
+# required to open dataset (gzip) and grab each data point (json) 
+import gzip, json
+
+# required to use 'deepcopy' command; taken from https://mubaris.com/posts/kmeans-clustering/
+from copy import deepcopy
+
 
 
 def vec(s):
-    return nlp.vocab[s].vector
+    """ takes in a word (string), outputs its vector representation """
+    # return nlp(s).vector # if loading sm spacy module
+    return nlp.vocab[s].vector # if loading md / lg spacy module
+
 
 def meanv(coords):
-    # assumes every item in coords has same length as item 0
-    sumv = [0] * len(coords[0])
+    """ takes in a list of N-dimensional vectors, outputs the mean vector 
+    (assumes every item in coords has same length as item 0) """
+    
+    # sumv = [0] * len(coords[0])     # gives a 0 vector of dimension = dim(coords[0])
+    sumv = [0] * wordDim              # 300 dimensional word vectors
     for item in coords:
         for i in range(len(item)):
             sumv[i] += item[i]
-    mean = [0] * len(sumv)
+    # mean = [0] * len(sumv)          # gives a 0 vector of dimension = dim(sumv) = dim(coords[0])
+    mean = [0] * wordDim
     for i in range(len(sumv)):
         mean[i] = float(sumv[i]) / len(coords)
     return mean
 
-def dist(a,b):
-     return numpy.linalg.norm(a-b)
+
+def dist(a, b, ax = 1):
+    """ takes in two N-dimensional vectors: a and b. Outputs the distance from a to b. """
+    return np.linalg.norm(a-b, axis = ax)
 
 
-# starts the timer
-start = timeit.default_timer()
+def maxNorm(vectors):
+    """ takes in a list of vectors; outputs tuple of vector with largest norm and its respective norm """
+    maxNorm = 0
+    maxVector = [0]*300
+    for vector in vectors:
+        norm = np.linalg.norm(vector)
+        if norm > maxNorm:
+            maxNorm = norm
+            maxVector = vector
 
-all_dataPoints = []
-i = 0
-while i < 500000:
-     for datapoint in gzip.open("gutenberg-poetry-v001.ndjson.gz"):
-          all_dataPoints.append(json.loads(datapoint.strip()))
-          i += 1
-
-all_lines = [datapoint['s'] for datapoint in all_dataPoints]     
-
-
-all_tokenizations = []
-for line in all_lines:
-     doc = nlp(line)
-     tokenizedLine = []
-     for token in doc:
-          if token.is_alpha:
-               tokenizedLine += [token.text]
-     all_tokenizations.append(tokenizedLine)
-
-print(all_tokenizations)     
+    return maxVector, maxNorm
 
 
-all_vectorizations = []
-for tokenList in all_tokenizations:
-     listOfTokenVectors = []
-     for token in tokenList:
-          tokenVec = vec(token)
-          listOfTokenVectors += [tokenVec]
-     all_vectorizations.append(listOfTokenVectors)
+if __name__ == '__main__':
+    """ the k-clustering algorithm """
+    
+    # ============= STEP 0: LOADING DATA ================= #
+    # ~30 - 45 sec.
 
-meanVectorList = []
-for vectorList in all_vectorizations:
-     sentenceVector = meanv(vectorList)
-     meanVectorList.append(sentenceVector)
+    start_step0 = timeit.default_timer()
 
+    all_dataPoints = []
+    
+    print('\n==> Loading poetry line data...')
+    
+    for datapoint in gzip.open("gutenberg-poetry-v001.ndjson.gz"):
+        all_dataPoints.append(json.loads(datapoint.strip()))
+        
+    all_poetryLines = [datapoint['s'] for datapoint in all_dataPoints]  # creates a list of just the poetry lines from the data points
 
-
-
-# K-MEANS CLUSTERING
-%matplotlib inline
-from copy import deepcopy
-from matplotlib import pyplot as plt
-plt.rcParams['figure.figsize'] = (16, 9)
-plt.style.use('ggplot')
-
-# Number of clusters
-k = 3
-# X coordinates of random centroids
-C_x = np.random.randint(0, np.max(X)-20, size=k)
-# Y coordinates of random centroids
-C_y = np.random.randint(0, np.max(X)-20, size=k)
-C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
-print(C)
+    stop_step0 = timeit.default_timer()
+    print('Time Elapsed While Loading Data (STEP 0): ', stop_step0 - start_step0,'sec') 
 
 
-## don't need to plot!
+    # ============= STEP 1: DATA -> VECTORS ================= #
+    # ~2 hrs...(for the 50,000 minimum)
+    # ~1 - 1.5 min...(for 5000 lines)
 
-# TODO: remember to take out all the plotting things!
+    start_step1 = timeit.default_timer()
 
-# # Getting the values and plotting it
-# f1 = data['V1'].values
-# f2 = data['V2'].values
-# X = np.array(list(zip(f1, f2)))
-# plt.scatter(f1, f2, c='black', s=7)
+    all_lineVectors = []
 
-# # Plotting along with the Centroids
-# plt.scatter(f1, f2, c='#050505', s=7)
-# plt.scatter(C_x, C_y, marker='*', s=200, c='g')
+    print('\n==> Converting list of poetry lines to list of vectors...')
+    lineTracker = 0 # communicates how many lines have been vectorized
+    maxLines = 5000 # 500,000 for the sake of minimum project requirement (otherwise loop takes forever)
+    checker = 1000 # have the comp holler back every 'checker' amount of lines tokenized
 
-# TODO: look through this K-clustering stuff (the real deal)
+    for poetryLine in all_poetryLines[:maxLines]: 
 
-# # To store the value of centroids when it updates
-# C_old = np.zeros(C.shape)
-# # Cluster Lables(0, 1, 2)
-# clusters = np.zeros(len(X))
-# # Error func. - Distance between new centroids and old centroids
-# error = dist(C, C_old, None)
-# # Loop will run till the error becomes zero
-# while error != 0:
-#     # Assigning each value to its closest cluster
-#     for i in range(len(X)):
-#         distances = dist(X[i], C)
-#         cluster = np.argmin(distances)
-#         clusters[i] = cluster
-#     # Storing the old centroid values
-#     C_old = deepcopy(C)
-#     # Finding the new centroids by taking the average value
-#     for i in range(k):
-#         points = [X[j] for j in range(len(X)) if clusters[j] == i]
-#         C[i] = np.mean(points, axis=0)
-#     error = dist(C, C_old, None)
+        lineVectorization = []
+        doc = nlp(poetryLine)
+        
+        for token in doc:
+            if token.is_alpha:
+                wordVec = vec(token.text)
+                lineVectorization += [wordVec]
+        
+        if len(lineVectorization) == 0:
+            continue
 
-# TODO: look through this stuff too. (from example 1 in https://mubaris.com/posts/kmeans-clustering/)
+        lineVec = meanv(lineVectorization)
+        all_lineVectors.append(lineVec)
 
-# from sklearn.cluster import KMeans
+        lineTracker += 1
+        if lineTracker % checker == 0:
+            print('\n ~ Only', maxLines - lineTracker, 'lines left! ~')
 
-# # Number of clusters
-# kmeans = KMeans(n_clusters=3)
-# # Fitting the input data
-# kmeans = kmeans.fit(X)
-# # Getting the cluster labels
-# labels = kmeans.predict(X)
-# # Centroid values
-# centroids = kmeans.cluster_centers_
+    all_lineVectors_array = np.array(all_lineVectors)
 
-# # Comparing with scikit-learn centroids
-# print(C) # From Scratch
-# print(centroids) # From sci-kit learn
+    stop_step1 = timeit.default_timer()
+    print('Time Elapsed While Converting Poetry Lines to Vectors (STEP 1) : ', stop_step1 - start_step1,'sec')  
 
-# TODO: this underneath is all plotting too
 
-# colors = ['r', 'g', 'b', 'y', 'c', 'm']
-# fig, ax = plt.subplots()
-# for i in range(k):
-#         points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
-#         ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
-# ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='#050505')
+    # ============= STEP 2: K-CLUSTERING ================= #
+    # ~2 min. (@K = 3; 'md' spacy module; 5000 poetry lines)
+    # ~2 min. (@K = 6; 'md' spacy module; 5000 poetry lines)
 
+    start_step2 = timeit.default_timer()
+
+    print('\n==> Commencing K-clustering algorithm...')
+
+    # calculating vector with largest norm and its norm
+    largestVector, largestNorm = maxNorm(all_lineVectors)
+        
+    # number of clusters
+    K = 6
+
+    # initializing the "list" (array) of all the labels (clusters) corresponding to each vector (poetry lines)
+    listOfClusterLabels = np.zeros(len(all_lineVectors_array))
+
+    # creating list of K randomly generated centroids
+    i = 0
+    listOfCentroids = []
+    listOfCentroids_old = []
+    for i in range(K):
+        C_i = np.random.randint(0, largestNorm, size = wordDim)
+        listOfCentroids.append(C_i)
+        listOfCentroids_old.append(np.zeros(wordDim))
+
+    Centroids = np.array(listOfCentroids)
+    Centroids_old = np.array(listOfCentroids_old)
+
+
+    # error func. - distance between new centroids and old centroids
+    error = dist(Centroids, Centroids_old, None)
+
+
+    loopTracker = 0
+    # loop will run till the error becomes zero
+    while error != 0:
+
+        # Assigning each value to its closest cluster
+        for i in range(len(all_lineVectors_array)):
+            
+            # gives us an array where each member tells us how faraway the data point is from each centroid
+            distances = dist(all_lineVectors_array[i], Centroids) 
+            
+            # this tells us which cluster (0, ..., K) the vector (aka: the poetry line) corresponds to
+            clusterLabel = np.argmin(distances)
+
+            # adding the label above to the list of all labels
+            listOfClusterLabels[i] = clusterLabel
+
+        # Storing the old centroid values
+        Centroids_old = deepcopy(Centroids)
+
+        # Finding the new centroids by taking the average value
+        for i in range(K):
+            points_ithCluster = [all_lineVectors_array[j] for j in range(len(all_lineVectors_array)) if listOfClusterLabels[j] == i]
+            Centroids[i] = np.mean(points_ithCluster, axis=0)
+        
+        error = dist(Centroids, Centroids_old, None)
+        loopTracker += 1
+        print('\n ~ ', loopTracker ,': Converging to better centroid values! ~')
+
+    print(Centroids)
+
+    stop_step2 = timeit.default_timer()
+    print('\nTime Elapsed While K-Clustering (STEP 2) : ', stop_step2 - start_step2,'sec') 
 
 
 
 # stops the timer
 stop = timeit.default_timer()
-print('Time: ', stop - start)  
-
-
-
-
-
+print('\nTotal Time Elapsed: ', stop - start,'sec')  
